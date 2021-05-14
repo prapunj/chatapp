bde83fadb817e51110fa139e5aa086dca3f0fc95
Prapunj Tiwari <prapunj.tiwari@intelliswift.com>
2021-05-14 16:48:43 +0530

    added connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path')
-
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname + '/../public')
 
-var app = express()
+let app = express()
+let server = http.createServer(app)
+let io = socketIO(server)
+
+io.on('connection',(socket)=>{
+    console.log("A new user just connected!!")
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected")
+    })
+})
+
 
 app.use(express.static(publicPath))
 
 
-app.listen(process.env.PORT || 8080,()=>{
+server.listen(process.env.PORT || 8080,()=>{
     console.log("listening at 8080")
 })
